c2a4bc27d128825ee65119395eb5f5823a20461e
Joseph Zebede <107275650+JayZeeeS@users.noreply.github.com>
2023-08-17 13:25:00 -0500

    Cleaned up packages

--- configuration.nix
+++ configuration.nix
@@ -64,12 +64,9 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # Programming
-    elixir
     gcc
     git
-    go
     neovim
-    python311
     vim
     zsh
     # Utility
